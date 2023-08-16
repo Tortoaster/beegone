6229ff3f2876f105e7158860c50fba926581bb3c
Rick van der Wal <rickvanderwalvis@gmail.com>
2023-08-16 11:48:30 +0200

    Add labels to pieces and actions

--- beegone_front_end/src/components/ActionButton.svelte
+++ beegone_front_end/src/components/ActionButton.svelte
@@ -13,10 +13,26 @@
 	}
 </script>
 
-<svg on:click={dispatchAction} viewBox="0 0 300 300">
+<svg class="group cursor-pointer" on:click={dispatchAction} viewBox="0 0 300 300">
 	{#if action.type === 'move'}
-		<circle class="cursor-pointer fill-black hover:fill-white" cx="150" cy="150" r="50" />
+		<circle class="fill-black group-hover:fill-white" cx="150" cy="150" r="50" />
+		<text
+			x="150"
+			y="150"
+			dominant-baseline="middle"
+			text-anchor="middle"
+			class="text-2xl font-bold select-none fill-white group-hover:fill-black">Move</text
+		>
 	{:else}
-		<circle class="cursor-pointer fill-black hover:fill-white" cx="150" cy="150" r="50" />
+		<circle class="fill-black group-hover:fill-white" cx="150" cy="150" r="50" />
+		<text
+			x="150"
+			y="150"
+			dominant-baseline="middle"
+			text-anchor="middle"
+			class="text-2xl font-bold select-none fill-white group-hover:fill-black"
+		>
+			{action.content.spawn.kind.type == 'bee' ? action.content.spawn.kind.content.species : 'Wall'}
+		</text>
 	{/if}
 </svg>
--- beegone_front_end/src/components/PieceComponent.svelte
+++ beegone_front_end/src/components/PieceComponent.svelte
@@ -5,9 +5,16 @@
 	import Token from './Token.svelte';
 
 	export let piece: Piece;
+
+	const VIEW_BOX = 360;
 </script>
 
-<svg class="cursor-pointer" in:receive={{ key: piece.id }} out:send={{ key: piece.id }}>
+<svg
+	viewBox="0 0 {VIEW_BOX} {VIEW_BOX}"
+	class="cursor-pointer"
+	in:receive={{ key: piece.id }}
+	out:send={{ key: piece.id }}
+>
 	{#if piece.kind.type === 'wall'}
 		<Tile
 			height={0.15}
@@ -23,7 +30,17 @@
 			height={0.1}
 			sideClass="fill-amber-400 dark:fill-slate-300"
 			topClass="fill-amber-300 dark:fill-slate-100"
-		/>
+		>
+			<text
+				x={VIEW_BOX / 2}
+				y={VIEW_BOX / 2}
+				dominant-baseline="middle"
+				text-anchor="middle"
+				class="text-4xl font-bold select-none fill-amber-900 dark:fill-slate-900"
+			>
+				{piece.kind.content.species}
+			</text>
+		</Token>
 	{:else}
 		<Token
 			on:click
@@ -31,6 +48,16 @@
 			height={0.1}
 			sideClass="fill-amber-900 dark:fill-slate-900"
 			topClass="fill-amber-800  dark:fill-slate-800"
-		/>
+		>
+			<text
+				x={VIEW_BOX / 2}
+				y={VIEW_BOX / 2}
+				dominant-baseline="middle"
+				text-anchor="middle"
+				class="text-4xl font-bold select-none fill-amber-300 dark:fill-slate-100"
+			>
+				{piece.kind.content.species}
+			</text>
+		</Token>
 	{/if}
 </svg>
